7947b8a21eb850e0af33ad02df24334012c40453
alan bivic <alan.bivic@hotmail.fr>
2019-11-18 14:08:31 +0100

    Ajout  de l'attribut Mission

--- projet181115v2/Modeles/Projet.cs
+++ projet181115v2/Modeles/Projet.cs
@@ -17,16 +17,18 @@ namespace projet181115v2.Modeles
         private DateTime _DateDebut;
         private DateTime _DateFin;
         private float _prixFacturéMO;
+        private Mission _lesMission;
 
         #endregion
 
         #region Constructeur
-        public Projet(string nom, DateTime DateDebut, DateTime DateFin, float prixFacturéMO)
+        public Projet(string nom, DateTime DateDebut, DateTime DateFin, float prixFacturéMO, Mission lesMission)
         {
             Nom = nom;
             this.DateDebut = DateDebut;
             this.DateFin = DateFin;
             PrixFacturéMO = prixFacturéMO;
+            LesMission = lesMission;
         }
 
 
@@ -37,6 +39,7 @@ namespace projet181115v2.Modeles
         public DateTime DateDebut { get => _DateDebut; set => _DateDebut = value; }
         public DateTime DateFin { get => _DateFin; set => _DateFin = value; }
         public float PrixFacturéMO { get => _prixFacturéMO; set => _prixFacturéMO = value; }
+        public Mission LesMission { get => _lesMission; set => _lesMission = value; }
         #endregion
 
         #region Methodes
