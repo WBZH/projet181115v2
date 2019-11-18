c42625a5d2970a5cf5a3a02d5f4f133fb6feee5b
alan bivic <alan.bivic@hotmail.fr>
2019-11-18 11:28:43 +0100

    Miaou

---
+++ projet181115v2/Modeles/ClassDeBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projet181115v2.Modeles
+{
+    class ClassDeBase
+    {
+
+        #region Attributs
+        //public static ArrayList CollClasseProduit = new ArrayList();
+        #endregion
+
+        #region Constructeur
+        //Produit.CollClasseProduit.Add(this);
+        #endregion
+
+        #region Getter Setter
+
+        #endregion
+
+        #region Methodes
+
+        #endregion
+        }
+    }
