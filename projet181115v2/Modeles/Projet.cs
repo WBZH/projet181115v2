d31949d235c4dd8369b703f1f8b79385e91fd42b
alan bivic <alan.bivic@hotmail.fr>
2019-11-18 14:05:33 +0100

    Classe Projet.cs de base

--- projet181115v2/Modeles/Projet.cs
+++ projet181115v2/Modeles/Projet.cs
@@ -10,15 +10,33 @@ namespace projet181115v2.Modeles
     class Projet
     {
         #region Attributs
+
         public static ArrayList CollClasseProjet = new ArrayList();
+
+        private string _nom;
+        private DateTime _DateDebut;
+        private DateTime _DateFin;
+        private float _prixFacturéMO;
+
         #endregion
 
         #region Constructeur
-        
+        public Projet(string nom, DateTime DateDebut, DateTime DateFin, float prixFacturéMO)
+        {
+            Nom = nom;
+            this.DateDebut = DateDebut;
+            this.DateFin = DateFin;
+            PrixFacturéMO = prixFacturéMO;
+        }
+
+
         #endregion
 
         #region Getter Setter
-
+        public string Nom { get => _nom; set => _nom = value; }
+        public DateTime DateDebut { get => _DateDebut; set => _DateDebut = value; }
+        public DateTime DateFin { get => _DateFin; set => _DateFin = value; }
+        public float PrixFacturéMO { get => _prixFacturéMO; set => _prixFacturéMO = value; }
         #endregion
 
         #region Methodes
