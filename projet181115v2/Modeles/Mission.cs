0b0735e0b970814e90c0f7099f6b5765776a4b6b
Thomas Gallou <thomas.gallou22@gmail.com>
2019-11-18 14:17:25 +0100

    tt

---
+++ projet181115v2/Modeles/Mission.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projet181115v2.Modeles
+{
+    class Mission
+    {
+        #region Attributs
+            private string _nom;
+            private string _description;
+            private int _nbHeursPrevues;
+            private Dictionary _releveHoraire;
+            private Intervenant Executant;
+        #endregion
+
+        #region Constructeurs
+        #endregion
+
+        #region Getters/Setters
+        #endregion
+
+        #region Methodes
+        #endregion
+    }
+}
